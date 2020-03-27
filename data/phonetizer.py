e692b7063d28963a89ff4383583a3ebc74c49350
arseniiv <arseniiv@gmail.com>
2020-03-27 18:33:54 +0500

    Word ID uniqueness checks, more data in DB

--- data/phonetizer.py
+++ data/phonetizer.py
@@ -1,7 +1,8 @@
-from typing import List, Tuple
+"""Makes the database from the plaintext dictionary."""
+from typing import List, Tuple, Iterable, Dict
 from re import sub as rsub
 import more_itertools as mit
-from sqlalchemy import create_engine, Table, Column, String, Integer
+from sqlalchemy import create_engine, Table, Column, String, Integer, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -54,7 +55,7 @@ def phonetize(word: str) -> str:
     word = rsub('([аэиыоу])и', '\\1йи', word)
     word = rsub(f'([{consonants_for_e}])и', '\\1ьи', word)
     word = word.replace('ы', 'и')
-    
+
     word = rsub('о([^_`])', 'а\\1', word)
     word = rsub('э([^_])', 'и\\1', word)
     word = rsub('о$', 'а', word)
@@ -74,29 +75,53 @@ def phonetize(word: str) -> str:
         word = rsub(f'{c}$', pairs[c], word)
 
     return word
-       
 
-class TransWord(Base):
+
+class TransWord(Base): # type: ignore
     __tablename__ = 'main'
-    word = Column(String, primary_key=True)
+    word = Column(String)
+    gram = Column(String)
     trans = Column(String)
+    word_id = Column(Integer, primary_key=True)
+    starred = Column(Boolean)
 
-    def __init__(self, word:str, trans:str):
+    def __init__(self, word: str, gram: str, trans: str, word_id: int, starred: bool) -> None:
         self.word = word
+        self.gram = gram
         self.trans = trans
+        self.word_id = word_id
+        self.starred = starred
 
-    def __repr__(self):
-        return f"{self.word}: {self.trans}"
+    def __repr__(self) -> str:
+        star = '*' if self.starred else ''
+        return f"#{self.word_id} {star}{self.word}: {self.trans} [{self.gram.trim()}]"
 
 
-def getEntries():
-    with open('zznj.txt', encoding='windows-1251') as file:
+# Checking IDs for uniqueness:
+ids: Dict[int, str] = dict()
+def ensure_id(raw_id: str, line: str) -> int:
+    if not raw_id.isdecimal():
+        print(f"[id] not decimal: {raw_id}. Line: {line}")
+    id_ = int(raw_id, base=10)
+    if id_ in ids:
+        print(f"[id] not unique. Line: {line}")
+        print(f"[id] original id line: {ids[id_]}")
+    ids[id_] = line
+    return id_
+
+def getEntries() -> Iterable[TransWord]:
+    with open('allwords.txt', encoding='windows-1251') as file:
         for line in file:
             parts = line.split('|')
             if len(parts) > 2:
-                word = parts[2].strip().lower().replace("'", "_")
-                trans = phonetize(word)
-                yield TransWord(word, trans)
+                raw_word = parts[0].strip().lower()
+                starred = raw_word.startswith('*')
+                word = raw_word[1:] if starred else raw_word
+                gram = f" {parts[1].strip().lower()} "
+                raw_trans = parts[2].strip().lower().replace("'", "_")
+                trans = phonetize(raw_trans)
+                word_id = ensure_id(parts[-1].strip(), line.strip())
+                yield TransWord(word, gram, trans, word_id, starred)
 
 
 if __name__ == "__main__":
@@ -105,18 +130,17 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     # clearing the db table
     session.query(TransWord).delete()
     session.commit()
-    
+
     # getting entries from file
     entries = getEntries()
-    unique_entries = mit.unique_everseen(entries, lambda e: e.word)
-    
+
     # populating the db table
-    chunks = mit.chunked(unique_entries, 100000)  
+    chunks = mit.chunked(entries, 100000)  
     for index, chunk in enumerate(chunks):
-        print(f'chunk {index}...')
+        print(f'chunk {index}... ({chunk[0].word} — {chunk[-1].word})')
         session.bulk_save_objects(chunk)
         session.commit()
