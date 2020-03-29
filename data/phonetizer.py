017adbe0ba7d7b37e6a3251ffa105c9ee069685a
formicant <formicant@gmail.com>
2020-03-29 21:25:28 +0700

    Naming convention.

--- data/phonetizer.py
+++ data/phonetizer.py
@@ -17,67 +17,67 @@ engine = create_engine('sqlite:///abcd.sqlite', echo=False)
 
 bounds = ' ,-'
 accents = "'`"
-plainVows   = 'ыэаоу'
-jotVows     = 'иеяёю'
-vowPhonemes = 'иэаоу'
+plain_vows   = 'ыэаоу'
+jot_vows     = 'иеяёю'
+vow_phonemes = 'иэаоу'
 signs = 'ъь'
-sonorantCons = 'ймнлр'
-unpairedUnvoicedCons = 'хцчщ'
-pairedVoicedCons   = 'вбзджг'
-pairedUnvoicedCons = 'фпстшк'
-softOnlyCons = 'йчщ'
-hardOnlyCons = 'жшц'
+sonorant_cons = 'ймнлр'
+unpaired_unvoiced_cons = 'хцчщ'
+paired_voiced_cons   = 'вбзджг'
+paired_unvoiced_cons = 'фпстшк'
+soft_only_cons = 'йчщ'
+hard_only_cons = 'жшц'
 
-vows = plainVows + jotVows
-cons = sonorantCons + pairedVoicedCons + unpairedUnvoicedCons + pairedUnvoicedCons
-softableCons = [c for c in cons if c not in hardOnlyCons]
-voiceableCons = pairedUnvoicedCons + pairedUnvoicedCons.upper()
-unvoiceableCons = pairedVoicedCons + pairedVoicedCons.upper()
-voicingCons = pairedVoicedCons[1:] + pairedVoicedCons[1:].upper()  # without ‘в’
-unvoicingCons = unpairedUnvoicedCons + unpairedUnvoicedCons.upper() + voiceableCons
+vows = plain_vows + jot_vows
+cons = sonorant_cons + paired_voiced_cons + unpaired_unvoiced_cons + paired_unvoiced_cons
+softable_cons = [c for c in cons if c not in hard_only_cons]
+voiceable_cons = paired_unvoiced_cons + paired_unvoiced_cons.upper()
+unvoiceable_cons = paired_voiced_cons + paired_voiced_cons.upper()
+voicing_cons = paired_voiced_cons[1:] + paired_voiced_cons[1:].upper()  # without ‘в’
+unvoicing_cons = unpaired_unvoiced_cons + unpaired_unvoiced_cons.upper() + voiceable_cons
 
 def change(from_: str, to: str) -> Callable[[str], str]:
-    changingDict = {key: value for key, value in zip(from_, to)}
-    return lambda s: changingDict[s]
+    changing_dict = {key: value for key, value in zip(from_, to)}
+    return lambda s: changing_dict[s]
 
-phonemize = change(vows, vowPhonemes + vowPhonemes)
-reductLess = change(vowPhonemes, 'ииаау')
-reductMore = change(vowPhonemes, 'ииииу')
-voice = change(voiceableCons, unvoiceableCons)
-unvoice = change(unvoiceableCons, voiceableCons)
+phonemize = change(vows, vow_phonemes + vow_phonemes)
+reductLess = change(vow_phonemes, 'ииаау')
+reductMore = change(vow_phonemes, 'ииииу')
+voice = change(voiceable_cons, unvoiceable_cons)
+unvoice = change(unvoiceable_cons, voiceable_cons)
 
 
 class PhonTransform:
-    def __init__(self, searchPattern: str, *rules: Dict[str, str]) -> None:
-        self.searchPattern = re.compile(searchPattern, re.VERBOSE)
-        ruleDict = {k: v for rule in rules for k, v in rule.items()}
-        self.ruleFunc = lambda match: ruleDict[match.group()]
+    def __init__(self, search_pattern: str, *rules: Dict[str, str]) -> None:
+        self.searchPattern = re.compile(search_pattern, re.VERBOSE)
+        rule_dict = {k: v for rule in rules for k, v in rule.items()}
+        self.ruleFunc = lambda match: rule_dict[match.group()]
     
-    def applyTo(self, word: str) -> str:
+    def apply_to(self, word: str) -> str:
         return self.searchPattern.sub(self.ruleFunc, word)
 
-phonTransforms = [
+phon_transforms = [
     # softness and stress
     PhonTransform(
         rf"""[{cons}]?[{vows}{signs}][{accents}]?  # optional consonant, then, vowel or sign, then, optional accent
-           | [{softOnlyCons}]                      # soft-only consonant that should be uppercased """,
+           | [{soft_only_cons}]                    # soft-only consonant that should be uppercased """,
         # vowel:
-        {f'{v}{a}': f'{phonemize(v).upper()}' for v in plainVows for a in accents},
-        {f'{jv}{a}': f'Й{phonemize(jv).upper()}' for jv in jotVows for a in accents},
-        {f'{v}': f'{reductLess(phonemize(v))}' for v in plainVows},
-        {f'{jv}': f'Й{reductMore(phonemize(jv))}' for jv in jotVows},
+        {f'{v}{a}': f'{phonemize(v).upper()}' for v in plain_vows for a in accents},
+        {f'{jv}{a}': f'Й{phonemize(jv).upper()}' for jv in jot_vows for a in accents},
+        {f'{v}': f'{reductLess(phonemize(v))}' for v in plain_vows},
+        {f'{jv}': f'Й{reductMore(phonemize(jv))}' for jv in jot_vows},
         # vowel + consonant:
         {f'{c}{v}{a}': f'{c}{phonemize(v).upper()}' for c in cons for v in vows for a in accents},
-        {f'{sc}{jv}{a}': f'{sc.upper()}{phonemize(jv).upper()}' for sc in softableCons for jv in jotVows for a in accents},
-        {f'{soc}{v}{a}': f'{soc.upper()}{phonemize(v).upper()}' for soc in softOnlyCons for v in vows for a in accents},
+        {f'{sc}{jv}{a}': f'{sc.upper()}{phonemize(jv).upper()}' for sc in softable_cons for jv in jot_vows for a in accents},
+        {f'{soc}{v}{a}': f'{soc.upper()}{phonemize(v).upper()}' for soc in soft_only_cons for v in vows for a in accents},
         {f'{c}{v}': f'{c}{reductLess(phonemize(v))}' for c in cons for v in vows},
-        {f'{sc}{jv}': f'{sc.upper()}{reductMore(phonemize(jv))}' for sc in softableCons for jv in jotVows},
-        {f'{soc}{v}': f'{soc.upper()}{reductMore(phonemize(v))}' for soc in softOnlyCons for v in vows},
+        {f'{sc}{jv}': f'{sc.upper()}{reductMore(phonemize(jv))}' for sc in softable_cons for jv in jot_vows},
+        {f'{soc}{v}': f'{soc.upper()}{reductMore(phonemize(v))}' for soc in soft_only_cons for v in vows},
         # consonant:
         {f'{c}{s}': f'{c}' for c in cons for s in signs},
-        {f'{sc}ь': f'{sc.upper()}' for sc in softableCons},
-        {f'{soc}{s}': f'{soc.upper()}' for soc in softOnlyCons for s in signs},
-        {f'{soc}': f'{soc.upper()}' for soc in softOnlyCons},
+        {f'{sc}ь': f'{sc.upper()}' for sc in softable_cons},
+        {f'{soc}{s}': f'{soc.upper()}' for soc in soft_only_cons for s in signs},
+        {f'{soc}': f'{soc.upper()}' for soc in soft_only_cons},
         # incorrect formating in the file:
         {f'{s}': f'' for s in signs},
         {f'{s}{a}': f'' for s in signs for a in accents},
@@ -85,26 +85,26 @@ phonTransforms = [
     ),
     # voicing and cluster simplification
     PhonTransform(
-        rf"""[{voiceableCons}]{{1,2}}(?=[{voicingCons}])         # unvoiced cluster before a voicing consonant
-           | [{unvoiceableCons}]{{1,2}}(?=[{unvoicingCons}]|\b)  # voiced cluster before an unvoicing consonant or word-finally
+        rf"""[{voiceable_cons}]{{1,2}}(?=[{voicing_cons}])         # unvoiced cluster before a voicing consonant
+           | [{unvoiceable_cons}]{{1,2}}(?=[{unvoicing_cons}]|\b)  # voiced cluster before an unvoicing consonant or word-finally
            # TODO: cluster simplification """,
         # voicing:
-        {f'{c}': f'{voice(c)}' for c in voiceableCons},
-        {f'{c1}{c2}': f'{voice(c1)}{voice(c2)}' for c1 in voiceableCons for c2 in voiceableCons},
+        {f'{c}': f'{voice(c)}' for c in voiceable_cons},
+        {f'{c1}{c2}': f'{voice(c1)}{voice(c2)}' for c1 in voiceable_cons for c2 in voiceable_cons},
         # unvoicing:
-        {f'{c}': f'{unvoice(c)}' for c in unvoiceableCons},
-        {f'{c1}{c2}': f'{unvoice(c1)}{unvoice(c2)}' for c1 in unvoiceableCons for c2 in unvoiceableCons},
+        {f'{c}': f'{unvoice(c)}' for c in unvoiceable_cons},
+        {f'{c1}{c2}': f'{unvoice(c1)}{unvoice(c2)}' for c1 in unvoiceable_cons for c2 in unvoiceable_cons},
         # cluster simplification:
         # TODO
     )
 ]
 
 # TODO: allow exceptions from this rule!
-ogoPattern = re.compile(r"((?:о|е)'?)г(о'?(?:ся)?)$")
+ogo_pattern = re.compile(r"((?:о|е)'?)г(о'?(?:ся)?)$")
 
 def phonetize(raw_trans: str) -> str:
-    raw_trans = ogoPattern.sub(r"\1в\2", raw_trans)
-    return ft.reduce(lambda w, t: t.applyTo(w), phonTransforms, raw_trans)
+    trans = ogo_pattern.sub(r"\1в\2", raw_trans)
+    return ft.reduce(lambda w, t: t.apply_to(w), phon_transforms, trans)
 
 
 class TransWord(Base): # type: ignore
